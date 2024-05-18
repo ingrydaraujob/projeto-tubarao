a89721d622e830e21a035a7a9067a374cb7f3324
Ury <117782130+Ury0@users.noreply.github.com>
2024-05-18 17:20:20 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -11,3 +11,49 @@ function carrossel(){
    imgs.style.transform = `translateX(${-idx*500}px)`
 }
 setInterval(carrossel,1800);
+
+function calcIdade(){
+   let atual = new Date().getFullYear()
+   iidade.innerHTML = Number(atual) - Number(iano.value)
+}
+
+const form = document.querySelector("#form")
+const name = document.querySelector("#inome")
+const email = document.querySelector("#iemail")
+const senha = document.querySelector("#isenha")
+
+form.addEventListener("submit", (event) =>{
+   event.preventDefault()
+   if(nome.value == ""){
+      alert("por favor preencha o seu nome")
+      return
+   }
+   if(email.value == "" || !isEmailValid(email.value)){
+      alert("por favor, preencha o seu email")
+      return
+   }
+   if(!validaSenha(senha.value,4)){
+      alert("digite a senha com no minimo 4 characteres")
+      return
+   }
+
+   form.submit()
+})
+
+function isEmailValid(email){
+   const emailrgx = new RegExp(
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+   )
+   if(emailrgx.test(email)){
+      return true
+   }else{
+      return false
+   }
+}
+function validaSenha(senha, minDigitos){
+   if(senha.length >= minDigitos){
+      return true
+   }else{
+      return false
+   }
+}
