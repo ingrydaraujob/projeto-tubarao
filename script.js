f69fd4f13423da4731e749f17835dd2f59578fbf
Ury <117782130+Ury0@users.noreply.github.com>
2024-05-22 09:55:29 -0300

    rodape

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ function calcIdade(){
    let atual = new Date().getFullYear()
    iidade.innerHTML = Number(atual) - Number(iano.value)
 }
-const form = document.getElementById("form")
-const nome = document.getElementById("inome")
-const email = document.getElementById("iemail")
-const senha = document.getElementById("isenha")
-const emailrgx = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+const form = document.getElementById("form");
+const nome = document.getElementById("inome");
+const email = document.getElementById("iemail");
+const senha = document.getElementById("isenha");
 
 
 form.addEventListener("submit",(event)=>{
    event.preventDefault();
-   verificaUsuario()
-   verificaEmail()
-   verificaSenha()
+
+   verificaUsuario();
+   verificaEmail();
+   verificaSenha();
 })
 
 function verificaUsuario(){
    
    if(nome.value === ""){
-      erroInput(nome,"preencha o nome de usuario","form-content erro")
+      erroInput(nome,"preencha o nome de usuario","form-content erro");
    }else{
-      erroInput(nome,"","form-content ok")
+      erroInput(nome,"","form-content ok");
       
    }
 
@@ -30,20 +30,20 @@ function verificaUsuario(){
 
 function verificaEmail(){
    
-   if(emailrgx.test(email.value)){
-      erroInput(email,"preencha o email","form-content erro")
+   if(email.value === ""){
+      erroInput(email,"preencha o email","form-content erro");
    }else{
-      erroInput(email,"","form-content ok")
+      erroInput(email,"","form-content ok");
       
    }
 
 }
 function verificaSenha(){
    const senhavalor =senha.value
-   if(senhavalor.length > 4){
-      erroInput(email,"preencha o email","form-content erro")
+   if(senhavalor === ""){
+      erroInput(senha,"preencha a senha","form-content erro");
    }else{
-      erroInput(email,"","form-content ok")
+      erroInput(senha,"","form-content ok");
       
    }
 
